a5ee7614408c545ce2ed77a8757f3bd3c3c84283
JuanRugna <juanrugna@hotmail.com>
2022-11-02 22:53:26 -0300

    Cambios

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import WorkImg from "../assets/WorkImg.jpg";
+import TrucksRoad from "../assets/TrucksRoad.JPG";
 import BioMountain from "../assets/BioMountain.JPG";
 import CalculadoraCoordenadas from "../assets/CalculadoraCoordenadas.JPG";
 import ToDoList from "../assets/ToDoList.JPG";
@@ -25,7 +25,7 @@ const Work = () => {
           {/*Grid item*/}
           <div
             style={{ backgroundImage: `url(${WeatherApp})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex text-center justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects*/}
             <div className="opacity-0 group-hover:opacity-100">
@@ -45,12 +45,16 @@ const Work = () => {
                   </button>
                 </a>
               </div>
+              <div className="text-sm pt-8 text-center font-bold text-white tracking-wider">
+              <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">ReactJS</span>
+              <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">WeatherApi</span>
+              </div>
             </div>
           </div>
           {/*Grid item 2*/}
           <div
             style={{ backgroundImage: `url(${BioMountain})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
             {/* Hover Effects*/}
             <div className="opacity-0 group-hover:opacity-100">
@@ -70,12 +74,20 @@ const Work = () => {
                   </button>
                 </a>
               </div>
+              <div className="text-sm pt-8 text-center font-bold text-white tracking-wider">
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">HTML</span>
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">CSS</span>
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">JavaScript</span>
+                <br />
+                
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 mt-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">Materialize</span>
+              </div>
             </div>
           </div>
           {/*Grid item 3*/}
           <div
             style={{ backgroundImage: `url(${ToDoList})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
           >
             {/* Hover Effects*/}
             <div className="opacity-0 group-hover:opacity-100">
@@ -95,12 +107,15 @@ const Work = () => {
                   </button>
                 </a>
               </div>
+              <div className="text-sm pt-8 text-center font-bold text-white tracking-wider">
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">ReactJS</span>
+              </div>
             </div>
           </div>
           {/*Grid item 4*/}
           <div
             style={{ backgroundImage: `url(${CalculadoraCoordenadas})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
           >
             {/* Hover Effects*/}
             <div className="opacity-0 group-hover:opacity-100">
@@ -120,30 +135,39 @@ const Work = () => {
                   </button>
                 </a>
               </div>
+              <div className="text-sm pt-8 text-center font-bold text-white tracking-wider">
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">ReactJS</span>
+              </div>
             </div>
           </div>
           {/*Grid item 5*/}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            style={{ backgroundImage: `url(${TrucksRoad})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
           >
             {/* Hover Effects*/}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                BioMountain 📦🌿
+                TrucksRoad
               </span>
 
               <div className="pt-8 text-center">
-                <a href="/"  rel="noreferrer" target="_blank">
+                <a href="https://www.figma.com/proto/F38UcTx9zNf30WfTH1iUAl/Trucks-Road?node-id=2%3A5&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=2%3A5"  rel="noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/"  rel="noreferrer" target="_blank">
+                <a href="https://github.com/JuanRugna/TrucksRoad"  rel="noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
+              </div>
+              <br />
+              <div className="text-sm pt-8 text-center font-bold text-white tracking-wider">
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">PHP</span>
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">MYSQL</span>
+                <span className="inline-block ml-3 px-2 py-1 font-semibold border-2 bg-stone-700 border-stone-900 dark:border-stone-900 rounded-md">Bootstrap</span>
               </div>
             </div>
           </div>
